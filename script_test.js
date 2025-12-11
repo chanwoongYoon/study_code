f3a37122cf572914d57b7bb98b41cee17a3befa8
chanwoongYoon <cwyoon9718@gamil.com>
2025-12-11 11:58:55 +0900

    잡다함

---
+++ script_test.js
@@ -0,0 +1,1 @@
+const numbers = ['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh','twelfth','thirteenth','fourteenth','fifteenth','sixteenth','seventeenth','eighteenth','nineteenth','twentieth'];
